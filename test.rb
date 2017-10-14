d5031ec6d7a0da7dcf545411daac898829423caa
takao-h <hayashi_takao@cyberbuzz.co.jp>
2017-10-14 15:02:48 +0900

    2017/10/14 Udemy API rewuest & get JOSN request

--- test.rb
+++ test.rb
@@ -1,10 +1,24 @@
-#apiリクエスト
-def reqedt_Api
+# 板情報の取得
 
+# ライブラリの指定
+require 'net/http'
+require 'uri'
+require 'json'
+# endpoint
+uri = URI.parse("https://api.bitflyer.jp")
+# api request_uri
+uri.path = '/v1/getboard'
+# クエリの選択
+uri.query = ''
 
-end
+# htpps通信の設定
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+response = https.get uri.request_uri
 
-#受け取った結果の整形
-def get_market_info
+# 吐き出し
+# puts response.body
 
-end
+# JSONに変換してカラムを指定
+response_hash = JSON.parse(response.body)
+puts response_hash["mid_price"]
